a59c72748c0a64a1b8f2435f67497de91c2afe59
turan <mturan.barut@gmail.com>
2023-10-22 16:20:59 +0300

    C#101-If-else

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace if_else;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int time=DateTime.Now.Hour;
+
+
+        if(time<=12 && time>=6){
+            Console.WriteLine("Günaydın.");
+        }else if(time>12 && time<18){
+            Console.WriteLine("iyi günler");
+        }else {
+            Console.WriteLine("iyi geceler.");
+        }
+
+        string sonuc=time<12 ? "Günaydın":"iyi geceler";
+        Console.WriteLine(sonuc);
+
+        sonuc=time>6 && time<12 ? "günaydın": time<18 ?"iyi günler":"iyi geceler";
+        Console.WriteLine(sonuc);
+
+
+
+    }
+    
+}
